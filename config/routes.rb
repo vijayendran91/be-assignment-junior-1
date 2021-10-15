0ade0178c52c708d3d781287da1229912dcb34be
Anbazhagan <anbublacky@gmail.com>
2021-10-15 12:06:47 +0530

    fix: Minor route changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "static#dashboard"
-  get 'people/:id', to: 'static#people'
+  get 'people/:id', to: 'static#person'
 end
